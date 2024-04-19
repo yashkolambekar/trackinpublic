90f034a8b59d6f5cd0ccdc52990ed0bf69b75aaf
Yash Kolambekar <yashskolambekar2015@gmail.com>
2024-04-20 03:46:57 +0530

    minor fix

--- src/components/Timeline/TimelineContainer/Day/Day.jsx
+++ src/components/Timeline/TimelineContainer/Day/Day.jsx
@@ -2,7 +2,7 @@ const Day = () => {
     return (
         <>  
             <div className="min-h-full  min-w-[10em] mr-4 border-2 border-[rgba(0,0,0,0.3)]">
-                <Task />
+                {/* <Task /> */}
             </div>
         </>
     )
